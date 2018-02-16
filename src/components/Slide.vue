d1fd25b85061a079bc5a32db6935364b48e34130
Maksim Khodyrev <maximkou@gmail.com>
2018-02-16 12:39:53 +0400

    # Add swipe # Add opacity: 0 to invisible slides

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,7 +1,7 @@
 <template>
-  <div :style="slideStyle" @click="gotoSlide()" class="carousel-slide">
+  <v-touch v-on:swipeleft="onSwipeLeft" v-on:swiperight="onSwipeRight" v-on:tap="gotoSlide" @click="gotoSlide" :style="slideStyle" class="carousel-slide">
     <slot></slot>
-  </div>
+  </v-touch>
 </template>
 
 <script>
@@ -20,23 +20,27 @@
       },
       computed: {
           slideStyle() {
-              let padding = 30,
+              let padding = this.getPadding(),
                   maxZIndex = 99,
                   maxVisibleSlides = 3,
-                  posRight = 0;
+                  posRight = 0,
+                  opacity = 1;
 
-              if (this.idx < maxVisibleSlides) {
-                  posRight = padding * (maxVisibleSlides - this.idx);
+              posRight = padding * (maxVisibleSlides - this.idx);
+
+              if (this.idx > maxVisibleSlides) {
+                  opacity = 0;
               }
 
               let styles = {
                   top: (this.idx - 1) * padding + 'px',
                   bottom: (this.idx - 1) * padding + 'px',
                   right: posRight + 'px',
+                  opacity: opacity,
                   'z-index': maxZIndex - this.idx,
               };
 
-              styles.transition = 'right 500ms, top 500ms, bottom 500ms, z-index 0ms';
+              styles.transition = 'right 500ms, top 500ms, bottom 500ms, z-index 0ms, opacity 150ms';
 
               return styles;
           }
@@ -46,8 +50,42 @@
             this.parent.goToSlide(this.idx);
           },
 
-          setIndex: function (index) {
+          setIndex(index) {
               this.idx = index;
+          },
+
+          onSwipeLeft() {
+              let nextIdx = this.idx + 1;
+
+              if (this.parent.slides.length < nextIdx) {
+                  nextIdx = 1;
+              }
+
+              this.parent.goToSlide(nextIdx);
+          },
+
+          onSwipeRight() {
+              let prevIdx = this.idx - 1;
+
+              if (prevIdx < 1) {
+                  prevIdx = this.parent.slides.length;
+              }
+
+              this.parent.goToSlide(prevIdx);
+          },
+
+          getPadding() {
+              let width = window.innerWidth;
+
+              if (width < 768) {
+                  return 10;
+              }
+
+              if (width < 1200) {
+                  return 20;
+              }
+
+              return 30;
           }
       }
   }
--- src/slider.js
+++ src/slider.js
@@ -4,8 +4,10 @@
 
 import Slider from './components/Slider.vue';
 import Slide from'./components/Slide.vue';
+import VTouch from 'vue-touch';
 
 const install = (Vue) => {
+    Vue.use(VTouch);
     Vue.component('cascade-slider', Slider);
     Vue.component('cascade-slider-slide', Slide);
 };
